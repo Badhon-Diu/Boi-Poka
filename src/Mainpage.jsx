ae24cd4b1b33af49cffc053a901ad9d395b7c558
Mahmud Badhon <139662753+Badhon-Diu@users.noreply.github.com>
2025-05-01 22:25:04 +0600

    first commit

---
+++ src/Mainpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const Mainpage = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  );
+};
+
+export default Mainpage;
---
+++ src/NavBar.jsx
@@ -0,0 +1,71 @@
+const NavBar = () => {
+  return (
+    <div className="md:p-4 p-2">
+      <div className="navbar bg-base-100  shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-2xl">Book Vibe</a>
+        </div>
+        <div className="navbar-center hidden lg:flex ">
+          <ul className="menu menu-horizontal md:flex md:gap-6 px-1 text-xl">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a href="">Listed Books</a>
+            </li>
+            <li>
+              <a>Pages to Read</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          <a className="btn rounded-xl bg-[#23be0a] p-6 text-white">Sign In</a>
+          <a className="btn  rounded-xl p-6 bg-[#59c6d2] text-white">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
